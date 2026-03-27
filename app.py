a48488ccf2b1d544f2b06e5a71ad52d3dd6828c0
Ubuntu <ubuntu@ip-172-31-93-0.ec2.internal>
2026-03-27 15:14:39 +0000

    update

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Սերվերը աշխատում է 5000 պորտի վրա:"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
