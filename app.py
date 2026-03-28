aebdb0b1b6d3765d4e7fc53979ec7000d2fad2d2
Ubuntu <ubuntu@ip-172-31-93-0.ec2.internal>
2026-03-28 17:55:38 +0000

    Fixo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Սերվերը աշխատում է 5000 պորտի վրա:🔥🥳💻"
+    return "Laurik jan bacatrem , sa cragir e grvac Yuroi koxmic, zarmacac es ete ayo greq instagram-in😂😂👌"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
